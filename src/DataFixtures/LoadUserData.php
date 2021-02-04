b0c3a90cb5a4c0123dd43817f00d1fe04863e1b7
Aleks <als.aleks@gmail.com>
2021-02-04 16:52:09 +0200

    api platform

--- src/DataFixtures/LoadUserData.php
+++ src/DataFixtures/LoadUserData.php
@@ -18,8 +18,8 @@ class LoadUserData extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $user = new User("Test");
-//        $user->setPassword('$2y$13$N2Ct43KmtqehxpF4NR3/3eNledPA6cOdbL2A1YR/wdVylBriT4ESC');
+        $user = new User();
+        $user->setUsername('Test');
         $user->setPassword( $this->encoder->encodePassword($user, 'testtest') );
         $user->setName('Вася Пупкин');
         $manager->persist($user);
